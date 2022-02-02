4fbb9e93aa721354b5db0d9fc0c36dd3c31680f6
Milos08 <milosbuha98@gmail.com>
2022-02-02 23:29:03 +0100

    Zapocet projekat, dodan samo 1 label i 1 button

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import the react and reactDom libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
+
+
+// Create a react component
+const App= () => {
+const ButtonText = {Text: 'Click me!'};
+const labelText = 'Enter name:'; 
+
+    return (
+    <div>
+     <label className="label" htmlFor="name">
+         {labelText}
+    </label>
+     <input id="name" type="text"/>
+     <button style={{backgroundColor: 'blue', color:'white'}}>
+         {ButtonText.Text}
+    </button>
+    </div>
+    );
+};
+
+
+// Take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector("#root")
+)
